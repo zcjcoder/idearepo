d598d148c5e8c889ddca43fa53883de3c694c837
zcj <zcj@163.com>
2019-08-06 18:49:10 +0800

    你好

--- worn/src/gitTest.java
+++ worn/src/gitTest.java
@@ -3,4 +3,9 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("git git git ");
     }
+
+    public void stst(){
+
+        System.out.println(11111);
+    }
 }
